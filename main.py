710251421a334978730043dd06af5f8e349fa550
Teo Orthlieb <inspirateur.pro@gmail.com>
2021-03-09 16:28:03 -0500

    it works

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
+from collections import deque
+import itertools
 import json
 import random
+import sys
+from tqdm import tqdm
+
+
+def shuffle_extend(elems, amount):
+	# shuffle and extends elems such that len(elems) >= n
+	elems = random.sample(elems, len(elems))
+	i = 0
+	while len(elems) < amount:
+		elems.append(elems[i])
+		i += 1
+	return elems
 
 
 with open("students.json", "r") as fstudents:
 	# <role, [students]>
 	students: dict = json.load(fstudents)
+	# we'll retain as many pitches as there is prog students
+	n = len(students["Prog"])
+	for _role in students:
+		students[_role] = shuffle_extend(students[_role], n)
 
 
 def generate_wishes():
@@ -24,70 +42,67 @@ def generate_wishes():
 			fwishes.write(", ".join(scores) + "\n")
 
 
-def shuffle_extend(elems, n):
-	# shuffle and extends elems such that len(elems) >= n
-	elems = random.sample(elems, len(elems))
-	i = 0
-	while len(elems) < n:
-		elems.append(elems[i])
-		i += 1
-	return elems
+def gale_shapley(pitches, wishes):
+	"""
+	:param pitches: [pitch idx] a subset of all pitches (specified by index)
+	:param wishes: <student, [pitch idx]> ranked by preference
+	:return: <pitch idx, <role, (student, rank)>> a solution
+	"""
+	# <pitch idx, <role: (student, rank)>>
+	res = {pitch: {} for pitch in pitches}
+	for role in students:
+		to_assign = deque(students[role])
+		while to_assign:
+			a = to_assign.popleft()
+			for i, pitch in enumerate(wishes[a]):
+				if pitch in res:
+					if role not in res[pitch]:
+						res[pitch][role] = (a, i)
+						break
+					b, r = res[pitch][role]
+					if i < r:
+						res[pitch][role] = (a, i)
+						to_assign.append(b)
+						break
+	return res
+
+
+def score(solution):
+	return sum(
+		rank+1
+		for staff in solution.values()
+		for _, rank in staff.values()
+	)
 
 
 def solve():
 	with open("wishes.csv", "r") as fwishes:
 		pitches = fwishes.readline().strip().split(", ")[1:]
+		# <student, [pitches idx]> ranked by preference
 		wishes = {}
 		for line in fwishes:
 			student, *scores = line.strip().split(", ")
-			wishes[student] = scores
+			wishes[student] = sorted(range(len(scores)), key=scores.__getitem__, reverse=True)
 
-	def fav_pitch(r, s):
-		return max(
-			filter(
-				lambda ps: ps[0] in res and r not in res[ps[0]],
-				zip(pitches, wishes[s])
-			),
-			key=lambda ps: ps[1]
-		)[0]
-
-	def least_staffed():
-		return min(
-			res.items(),
-			key=lambda p_staff: len(p_staff[1])
-		)[0]
-
-	# <pitch, {role: student}>
-	res = {pitch: {} for pitch in pitches}
-
-	# for now, we'll retain as many pitches as there is prog students
-	amount = len(students["Prog"])
-	assert len(pitches) >= amount
-	while True:
-		# assign the students to the remaining pitches
-		for role, group in students.items():
-			group = shuffle_extend(group, amount)
-			for student in group:
-				pitch = fav_pitch(role, student)
-				res[pitch][role] = student
-		if len(res) == amount:
-			break
-		pitch = least_staffed()
-		del res[pitch]
-		# reset the staff on every pitch
-		res = {pitch: {} for pitch in res}
+	min_score = float("+inf")
+	best = None
+	for sub_pitches in tqdm(
+			itertools.combinations(range(len(pitches)), n), desc="Brutefoce", file=sys.stdout
+	):
+		res = gale_shapley(sub_pitches, wishes)
+		new_score = score(res)
+		if new_score < min_score:
+			min_score = new_score
+			best = res
 
 	# display the results
-	pmap = {pitch: i for i, pitch in enumerate(pitches)}
-
-	def get_score(s, p):
-		return wishes[s][pmap[p]]
-
-	for pitch, staff in res.items():
-		print(pitch)
-		for role, student in staff.items():
-			print(f"  - {role}: {student} ({get_score(student, pitch)})")
-	return res
+	for pitch, staff in best.items():
+		print(pitches[pitch])
+		for role, (student, rank) in staff.items():
+			print(f"  - {role}: {student} ({rank+1})")
+		print()
+	print("Total score:", min_score)
+	return best
 
 
 if __name__ == '__main__':
